897c5e69a2b2f3d2bfce0fb814404df5f2ecf4e2
Ganlin Zhang <zgl0315@gmail.com>
2021-12-22 15:13:54 +0100

    small error fix in run_test_img.sh

--- run_test_img.sh
+++ run_test_img.sh
@@ -5,8 +5,7 @@ python Test_img.py --KITTI 2015 \
                --loadmodel ./trained/checkpoint_99.tar \
                --model stackhourglass \
                --maxdisp 192 \
-               --leftimg dataset/data_scene_flow_2015/testing/image_2/000006_10.png \ 
+               --leftimg dataset/data_scene_flow_2015/testing/image_2/000006_10.png \
                --rightimg dataset/data_scene_flow_2015/testing/image_3/000006_10.png \
-               
                
                #--loadmodel ./trained/checkpoint_10.tar \
