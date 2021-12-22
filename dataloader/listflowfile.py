de312467d3e076cc5c0aed01b996abbd35bd75d3
Ganlin Zhang <zgl0315@gmail.com>
2021-12-22 20:41:21 +0100

    update dataloader to deal with driving dataset

--- dataloader/listflowfile.py
+++ dataloader/listflowfile.py
@@ -85,8 +85,11 @@ def dataloader(filepath):
     driving_dir = filepath + [x for x in image if 'driving' in x][0] + '/'
     driving_disp = filepath + [x for x in disp if 'driving' in x][0]
 
-    subdir1 = ['35mm_focallength','15mm_focallength']
-    subdir2 = ['scene_backwards','scene_forwards']
+    # subdir1 = ['35mm_focallength','15mm_focallength']
+    # subdir2 = ['scene_backwards','scene_forwards']
+
+    subdir1 = ['35mm_focallength']
+    subdir2 = ['scene_forwards']
     subdir3 = ['fast','slow']
 
     for i in subdir1:
