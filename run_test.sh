b4bce651f55194e7542404b9aa865957c6301147
Ganlin Zhang <zhangga@eu-login-17.euler.ethz.ch>
2021-12-24 10:26:20 +0100

    test img upload

--- run_test.sh
+++ run_test.sh
@@ -3,7 +3,7 @@
 python main.py --maxdisp 192 \
                --model stackhourglass \
                --datapath /cluster/scratch/zhangga/dataset/ \
-               --epochs 1 \
+               --epochs 50 \
                --savemodel ./trained/ \
                --batchsize 8 \
                --numworker 4 \
--- run_test_img.sh
+++ run_test_img.sh
@@ -2,10 +2,10 @@
 
 python Test_img.py --KITTI 2015 \
                --datapath ./dataset/data_scene_flow_2015/testing/ \
-               --loadmodel ./trained/checkpoint_99.tar \
+               --loadmodel ./trained/checkpoint_49.tar \
                --model stackhourglass \
                --maxdisp 192 \
-               --leftimg dataset/data_scene_flow_2015/testing/image_2/000006_10.png \
-               --rightimg dataset/data_scene_flow_2015/testing/image_3/000006_10.png \
+               --leftimg dataset/data_scene_flow_2015/training/image_2/000006_10.png \
+               --rightimg dataset/data_scene_flow_2015/training/image_3/000006_10.png \
                
                #--loadmodel ./trained/checkpoint_10.tar \
