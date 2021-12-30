27eb4f723cd8a3911d98be64f94034eb9c9c14cc
Ganlin Zhang <zgl0315@gmail.com>
2021-12-30 16:37:01 +0100

    test1

--- run_test1.sh
+++ run_test1.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 python main.py --maxdisp 192 \
-               --model dilated \
+               --model stackhourglass \
                --datapath /cluster/scratch/zhangga/dataset/ \
-               --epochs 50 \
-               --savemodel /cluster/scratch/zhangga/trained/dilated/ \
+               --epochs 30 \
+               --savemodel /cluster/scratch/zhangga/trained/new_psm/ \
+               --loadmodel /cluster/scratch/zhangga/trained/new_psm/checkpoint_49.tar \
                --batchsize 8 \
                --numworker 4 \
-               --startepoch 0 \
+               --startepoch 50 \
              
                
 	       #--loadmodel ./trained/gwc_dilated_seg/checkpoint_30.tar \
