5b6a178ba9871df31c8794cf9feed9967443dc77
Ganlin Zhang <zgl0315@gmail.com>
2021-12-31 09:49:42 +0100

    finetune bash

--- run_test2.sh
+++ run_test2.sh
@@ -4,7 +4,7 @@ python finetune.py --maxdisp 192 \
                --model stackhourglass \
                --datapath /cluster/scratch/zhangga/dataset/ \
                --epochs 50 \
-               --savemodel /cluster/scratch/zhangga/finetune/ \
+               --savemodel /cluster/scratch/zhangga/finetune \
                --loadmodel /cluster/scratch/zhangga/trained/seg/checkpoint_79.tar \
                --batchsize 8 \
                --numworker 4 \
--- run_test6.sh
+++ run_test6.sh
@@ -4,7 +4,7 @@ python finetune.py --maxdisp 192 \
                --model dilated \
                --datapath /cluster/scratch/xishen/dataset/ \
                --epochs 50 \
-               --savemodel /cluster/scratch/xishen/finetune/ \
+               --savemodel /cluster/scratch/xishen/finetune \
                --loadmodel /cluster/scratch/xishen/trained/gwc_dilated/checkpoint_79.tar \
                --batchsize 8 \
                --numworker 4 \
