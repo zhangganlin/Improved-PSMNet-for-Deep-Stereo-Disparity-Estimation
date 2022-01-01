236a36d89818bd71499b83ea0b968d2bbcdaafba
Ganlin Zhang <zgl0315@gmail.com>
2022-01-01 09:18:50 +0100

    run test 8

--- run_test8.sh
+++ run_test8.sh
@@ -3,11 +3,11 @@
 python finetune.py --maxdisp 192 \
                --model dilated \
                --datapath /cluster/scratch/xishen/dataset/ \
-               --epochs 100 \
+               --epochs 200 \
                --savemodel /cluster/scratch/xishen/finetune \
-               --loadmodel /cluster/scratch/xishen/trained/dilated_gwc_seg/checkpoint_79.tar \
+               --loadmodel /cluster/scratch/xishen/finetune/kitticheckpoint_99dilated_gwc_seg.tar \
                --batchsize 8 \
                --numworker 4 \
-               --startepoch 0 \
+               --startepoch 100 \
                --seg \
                --gwc \
